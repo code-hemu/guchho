d26de53435128a0a90802e402581d54ff6d4f7b8
hemanta-gayen <hematagayan2000@gmail.com>
2026-08-07 04:47:55 +0900

    test update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
-#include "guchho/helpers.hpp"
+
 
 #include <iostream>
 
 int main()
 {
-    bool result = guchho::helpers::ContainsNonBMPCodePoint("Hello 😀");
 
-    std::cout << std::boolalpha << result << '\n';
-
-    return 0;
 }
